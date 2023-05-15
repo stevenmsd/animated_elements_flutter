fd3c9f934ac987e2c99e3f747a9ad6e3642c16de
Steeven Montalvo <estywen@gmail.com>
2023-05-14 21:45:50 -0500

    feat:Animación formulario de ingreso

--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -74,7 +74,12 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 btnAnimationController: _btnAnimationController,
                 press: () {
                   _btnAnimationController.isActive = true;
-                  customSignInDialog(context);
+                  Future.delayed(
+                    const Duration(milliseconds: 800),
+                    () {
+                      customSignInDialog(context);
+                    },
+                  );
                 },
               ),
               const Padding(
@@ -94,6 +99,17 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         barrierDismissible: true,
         barrierLabel: "Sign In",
         context: context,
+        transitionDuration: const Duration(milliseconds: 400),
+        transitionBuilder: (_, animation, __, child) {
+          Tween<Offset> tween;
+          tween = Tween(begin: const Offset(0, -1), end: Offset.zero);
+          return SlideTransition(
+            position: tween.animate(
+              CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+            ),
+            child: child,
+          );
+        },
         pageBuilder: (context, _, __) => Center(
               child: Container(
                 height: 620,
@@ -106,69 +122,88 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 ),
                 child: Scaffold(
                   backgroundColor: Colors.transparent,
-                  body: Column(
+                  body: Stack(
+                    clipBehavior: Clip.none,
                     children: [
-                      const Text(
-                        "Sign In",
-                        style: TextStyle(fontSize: 34, fontFamily: "Poppins"),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 16),
-                        child: Text(
-                          "Access to 240+ hours pf content. Learn design and code, by building real apps with Flutter and Swift",
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      const SignInForm(),
-                      Row(
-                        children: const [
-                          Expanded(child: Divider()),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
+                      Column(
+                        children: [
+                          const Text(
+                            "Sign In",
+                            style:
+                                TextStyle(fontSize: 34, fontFamily: "Poppins"),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16),
                             child: Text(
-                              "OR",
-                              style: TextStyle(color: Colors.black26),
+                              "Access to 240+ hours pf content. Learn design and code, by building real apps with Flutter and Swift",
+                              textAlign: TextAlign.center,
                             ),
                           ),
-                          Expanded(child: Divider())
+                          const SignInForm(),
+                          Row(
+                            children: const [
+                              Expanded(child: Divider()),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 16),
+                                child: Text(
+                                  "OR",
+                                  style: TextStyle(color: Colors.black26),
+                                ),
+                              ),
+                              Expanded(child: Divider())
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              "Sign up with Email, Apple or Google",
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {},
+                                  icon: SvgPicture.asset(
+                                    "assets/icons/email_box.svg",
+                                    height: 64,
+                                    width: 64,
+                                  )),
+                              IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {},
+                                  icon: SvgPicture.asset(
+                                    "assets/icons/apple_box.svg",
+                                    height: 64,
+                                    width: 64,
+                                  )),
+                              IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {},
+                                  icon: SvgPicture.asset(
+                                    "assets/icons/google_box.svg",
+                                    height: 64,
+                                    width: 64,
+                                  )),
+                            ],
+                          )
                         ],
                       ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: Text(
-                          "Sign up with Email, Apple or Google",
-                          style: TextStyle(color: Colors.black54),
+                      const Positioned(
+                        left: 0,
+                        right: 0,
+                        bottom: -48,
+                        child: CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.close,
+                            color: Colors.black,
+                          ),
                         ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                "assets/icons/email_box.svg",
-                                height: 64,
-                                width: 64,
-                              )),
-                          IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                "assets/icons/apple_box.svg",
-                                height: 64,
-                                width: 64,
-                              )),
-                          IconButton(
-                              padding: EdgeInsets.zero,
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                "assets/icons/google_box.svg",
-                                height: 64,
-                                width: 64,
-                              )),
-                        ],
-                      )
                     ],
                   ),
                 ),
